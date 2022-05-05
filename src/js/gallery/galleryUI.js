760c4d660b06fac4e5e833774f9ed00b8eb102c2
Yurii Dvornichenko <raen98@live.ru>
2022-05-05 18:24:07 +0300

    2

--- src/js/gallery/galleryUI.js
+++ src/js/gallery/galleryUI.js
@@ -1,18 +1,22 @@
-export function renderSingleCountryInfo({}) {
+export function renderSingleCountryInfo({ webformatURL, tags, likes, views, comments, downloads }) {
   return ` <div class="photo-card">
-  <img src="" alt="" loading="lazy" />
+  <img src="${webformatURL}" alt="${tags}" loading="lazy" />
   <div class="info">
     <p class="info-item">
       <b>Likes</b>
+      ${likes}
     </p>
     <p class="info-item">
       <b>Views</b>
+      ${views}
     </p>
     <p class="info-item">
       <b>Comments</b>
+      ${comments}
     </p>
     <p class="info-item">
       <b>Downloads</b>
+      ${downloads}
     </p>
   </div>
 </div>
--- src/js/index.js
+++ src/js/index.js
@@ -1,6 +1,7 @@
-import axios from 'axios';
 import Notiflix from 'notiflix';
-import getPicture from '../js/helpers/axiosGetGallery';
+import NewsApiService from '../js/helpers/axiosGetGallery';
+import { renderSingleCountryInfo } from '../js/gallery/galleryUI';
+import LoadMoreBtn from '../js/components/load-more-btn';
 
 import '../sass/main.scss';
 
@@ -8,31 +9,74 @@ const refs = {
   searchForm: document.querySelector('.search-form'),
   searchFormInput: document.querySelector('.search-form input'),
   gallery: document.querySelector('.gallery'),
-  loadMoreBtn: document.querySelector('.load-more'),
 };
 
-refs.searchForm.addEventListener('submit', valueSearch);
+const newsApiService = new NewsApiService();
+const loadMoreBtn = new LoadMoreBtn({
+  selector: '.load-more',
+  hidden: true,
+});
 
-async function valueSearch(e) {
+refs.searchForm.addEventListener('submit', onSearchValue);
+loadMoreBtn.refs.button.addEventListener('click', onSearchMore);
+
+async function onSearchValue(e) {
   e.preventDefault();
 
+  newsApiService.query = refs.searchFormInput.value;
+
+  if (newsApiService.query === '') {
+    Notiflix.Notify.failure(
+      'Sorry, there are no images matching your search query. Please try again.',
+    );
+    return infoNotify();
+  }
+
+  newsApiService.resetPage();
+  clearGalleryContainer();
+  onSearchMore();
+  loadMoreBtn.hide();
+}
+
+async function onSearchMore() {
   try {
-    const query = refs.searchFormInput.value;
-    const items = await getPicture(query);
-    // console.log('items', items);
-    clearGalleryContainer();
+    const items = await newsApiService.getPicture();
+
+    const limitPage = items.data.totalHits / newsApiService.limit;
+
+    if (newsApiService.page > limitPage && limitPage >= 1) {
+      return infoNotify();
+    } else if (newsApiService.page === 2 && newsApiService.page > limitPage) {
+      return infoNotify();
+    }
+
+    showGalletyItems(items.data.hits);
+    newsApiService.incrementPage();
 
     if (items.data.hits.length === 0) {
       Notiflix.Notify.failure(
         'Sorry, there are no images matching your search query. Please try again.',
       );
+      loadMoreBtn.hide();
       return;
     }
   } catch (error) {
     console.log('Error');
   }
+
+  loadMoreBtn.show();
+}
+
+function showGalletyItems(items) {
+  const cards = items.map(item => renderSingleCountryInfo(item)).join('');
+  refs.gallery.insertAdjacentHTML('beforeend', cards);
 }
 
 function clearGalleryContainer() {
   refs.gallery.innerHTML = '';
+}
+
+function infoNotify() {
+  loadMoreBtn.hide();
+  Notiflix.Notify.info("We're sorry, but you've reached the end of search results.");
 }
